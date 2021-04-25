8063dd90eccc542491c868eb293c9044ed8a36f0
SturatParker <SturatParker@users.noreply.github.com>
2021-04-25 19:30:52 +0100

    guards

---
+++ src/database/guard.ts
@@ -0,0 +1,70 @@
+import {
+  Document,
+  DocumentDefinition,
+  Model,
+  Schema,
+  SchemaDefinition,
+  SchemaOptions,
+} from 'mongoose';
+
+export function guard<
+  T extends Document,
+  // F extends keyof T,
+  D extends Document
+>(
+  this: T,
+  // field: F,
+  value: Schema.Types.ObjectId | D
+): value is D {
+  const field = fieldName(this, value);
+  if (!field) return false;
+  return this.populated(field as string);
+}
+
+export interface IGuarded {
+  guard: <
+    T extends Document & IGuarded,
+    // F extends keyof T,
+    D extends Document
+  >(
+    this: T,
+    // field: F,
+    value: Schema.Types.ObjectId | D
+  ) => value is D;
+}
+
+export class GuardSchema<
+  DocType extends Document & IGuarded,
+  M extends Model<DocType, any> = Model<any, any>,
+  SchemaDefinitionType = undefined
+> extends Schema {
+  constructor(
+    definition?: SchemaDefinition<DocumentDefinition<SchemaDefinitionType>>,
+    options?: SchemaOptions
+  ) {
+    super(definition, options);
+    this.method('guard', guard);
+  }
+
+  plugin(
+    fn: (
+      schema: GuardSchema<DocType, M, SchemaDefinitionType>,
+      opts?: any
+    ) => void,
+    opts?: any
+  ): this {
+    return super.plugin(fn, opts);
+  }
+}
+
+function fieldName<D extends Document>(
+  object: D,
+  field: unknown
+): keyof D | undefined {
+  let k: keyof D;
+  for (let key in object) {
+    k = key as keyof D;
+    if (object[k] == field) return k;
+  }
+  return;
+}
